f8e383bcf782c68e2b60319c12edf496696478b9
MarcusMunch <marcus@marcusmunch.dk>
2018-07-28 20:39:41 +0200

    Created scraper

---
+++ main.py
@@ -0,0 +1,51 @@
+import urllib2
+import os
+
+from bs4 import BeautifulSoup
+
+
+def get_riders(link):
+    response = urllib2.urlopen(link)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    rider_list = soup.find("div", {"class": "list--competitors"})
+
+    for rider in rider_list.find_all("span", {"class": "runner"}):
+        link = rider.a['href']
+        name = link.split('/')[5]
+        persona = rider.text.strip().title()
+        team = link.split('/')[4]
+
+        yield {"link": link,
+               "name": name,
+               "persona": persona.encode("utf-8"),
+               "team": team}
+
+
+def collect_rider(rider):
+    response = urllib2.urlopen("https://www.letour.fr{}".format(rider["link"]))
+    html = response.read()
+
+    print("Now writing {}...".format(rider["persona"])),
+
+    with open("data/{}/{}.html".format(rider['team'], rider['name']), 'w') as f:
+        f.write(html)
+
+    print("Done!")
+
+
+def main():
+    riders = get_riders("https://www.letour.fr/en/riders")
+
+    for rider in riders:
+        team_path = "./data/" + rider['team']
+        if not os.path.exists(team_path):
+            os.mkdir(team_path)
+        collect_rider(rider)
+
+
+if __name__ == "__main__":
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    main()
